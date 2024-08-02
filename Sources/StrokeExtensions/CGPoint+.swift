7d298bf442ec44ba87ca09e0aecdcb759ee7f53e
Chris Davis <chris@nthstate.com>
2024-08-02 11:31:51 +0100

    chore: Update angle between method

--- Sources/StrokeExtensions/CGPoint+.swift
+++ Sources/StrokeExtensions/CGPoint+.swift
@@ -10,15 +10,15 @@
 import CoreGraphics
 
 public extension CGPoint {
-  
+
   static func * (point: CGPoint, size: CGSize) -> CGPoint {
     return CGPoint(x: point.x * size.width, y: point.y * size.height)
   }
-  
+
 }
 
 internal extension CGPoint {
-  
+
   static func * (point: CGPoint, size: CGFloat) -> CGPoint {
     return CGPoint(x: point.x * size, y: point.y * size)
   }
@@ -26,31 +26,31 @@ internal extension CGPoint {
   static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
     return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
   }
-  
+
   static func / (point: CGPoint, size: CGSize) -> CGPoint {
     return CGPoint(x: point.x / size.width, y: point.y / size.height)
   }
-  
+
   func magnitude() -> CGFloat {
     return sqrt(x * x + y * y)
   }
-  
+
   func distance(to: CGPoint) -> CGFloat {
     return CGPoint(x: to.x - x, y: to.y - y).magnitude()
   }
-  
+
   func angle(between finishPoint: CGPoint) -> CGFloat {
     let center = CGPoint(x: finishPoint.x - self.x, y: finishPoint.y - self.y)
     let radians = atan2(center.y, center.x)
     let degrees = radians * 180 / .pi
-    return degrees > 0 ? degrees : degrees + degrees
+    return degrees >= 0 ? degrees : degrees + 360
   }
-  
+
   func normalize() -> CGPoint {
     let s = 1.0 / magnitude()
     return self * s
   }
-  
+
   func rotateLeft() -> CGPoint {
     CGPoint(x: -self.y, y: self.x)
   }
